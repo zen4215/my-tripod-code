37674c01eb650bae742a73460b2c6507bd76168d
zen4215 <44k1.373@gmail.com>
2025-11-12 12:43:55 +0900

    feat: setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <Arduino.h>
+#include <IcsHardSerialClass.h>
 
+// set constant
+namespace ics{
+    const byte EN_PIN   {2};
+    const long BAUDRATE {115200};
+    const int  TIMEOUT  {10};
+    
+    IcsHardSerialClass krs(&Serial, EN_PIN, BAUDRATE, TIMEOUT);
+}
+
+const int   SERIAL_BAUDRATE {115200};
+const char* PS4_BLE_ADDLESS {"08:B6:1F:ED:4B:E2"};
+
+// setup function
 void setup() {
+    Serial.begin(SERIAL_BAUDRATE);
 }
 
 void loop() {
