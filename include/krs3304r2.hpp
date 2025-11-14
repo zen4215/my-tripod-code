edef719ccd7aa623f346556040b49927dbd3c4e6
zen4215 <44k1.373@gmail.com>
2025-11-14 12:45:28 +0900

    add: krs3304r2 class

--- include/krs3304r2.hpp
+++ include/krs3304r2.hpp
@@ -4,5 +4,16 @@
 
 class KRS3304R2 {
     public:
-    KRS3304R2(IcsHardSerialClass ics);
+    KRS3304R2(IcsHardSerialClass& ics, int id);
+    
+    private:
+    IcsHardSerialClass m_ics;
+    int m_id;
 };
+
+KRS3304R2::KRS3304R2(IcsHardSerialClass& ics, int id) :
+    m_ics(ics),
+    m_id(id)
+{
+    
+}
